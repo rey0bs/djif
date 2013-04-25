eb269790373f903d6c2c458534809e63974c8d7a
Simon Boyer <sboyer@Scrameustache>
2013-04-26 01:02:54 +0200

    Design form

--- template/form.php
+++ template/form.php
@@ -1,23 +1,34 @@
-<form action="/new" method="post">
-	<center>
-		Gif : <input id="gifSource" type="text" name="gif_url"><br>
-		<p>+</p>
-		sound : <input id="soundSource" type="text" name="audio_url"><br>
-		<p>=</p>
-		<input id="submit" type="submit" value="Remix !"/>
-	</center>
-	<script>
-		$('form').submit(function() {
-			$.ajax({
-				type: "POST",
-				url: "/new",
-				data: { gif_url: $('#gifSource').val(),
-								audio_url: $('#soundSource').val(),
-								ajax : true	}
-			}).done(function( output ) {
-				$('#action').append( output );
-			});
-			return false;
-		});
-	</script>
+<form action="/new" method="post" id="new_djif">
+
+	<div class="input_group gif_group">
+		<span class="iconic camera"></span>
+		<label for="gif_url">Gif</label>
+		<input id="gifSource" type="text" name="gif_url" onclick="select()" />
+	</div>
+
+	<span class="iconic plus_alt"></span>
+
+	<div class="input_group sound_group">
+		<span class="iconic play_alt"></span>
+		<label for="audio_url">Sound</label>
+		<input id="soundSource" type="text" name="audio_url" onclick="select()" />
+	</div>
+
+	<input id="remix" type="submit" value="Remix !"/>
+
 </form>
+<script>
+	$('form').submit(function() {
+		$.ajax({
+			type: "POST",
+			url: "/new",
+			data: { gif_url: $('#gifSource').val(),
+							audio_url: $('#soundSource').val(),
+							ajax : true	}
+		}).done(function( output ) {
+			$('#action').append( output );
+		});
+		return false;
+	});
+</script>
+
--- template/header.php
+++ template/header.php
@@ -1,6 +1,8 @@
 <html>
 	<head>
 		<meta charset='utf-8'>
+		
+		<link rel="stylesheet" type="text/css" href="typo/iconic_stroke/iconic_stroke.css">
 		<link rel="stylesheet" type="text/css" href="css/normalize.css">
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 		<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
