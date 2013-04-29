8cc86545e8b84be86d33db02e00a6c4dfdbd2468
gbrenon <gregoire.brenon@openwide.fr>
2013-04-29 16:38:37 +0200

    Added tool to generate random hashes

--- action/new.php
+++ action/new.php
@@ -2,5 +2,12 @@
 	$gif_url = $_POST['gif_url'];
 	$audio_url = $_POST['audio_url'];
 	
+	$charset = array_merge(range(0,9), range('a','z'), range('A', 'Z'));
+	$hash = '';
+	for ($i=0; $i < 5; $i++) {
+		$hash .= $charset[array_rand($charset)];
+	}
+
+	echo("will get tag $hash");
 	include('action/view.php');
 ?>
