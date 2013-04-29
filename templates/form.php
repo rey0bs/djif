575a3d591738d432b0c0740176d7b53ea163b646
Simon Boyer <simon.boyer@openwide.fr>
2013-04-29 14:47:20 +0200

    Help text on form

--- templates/form.php
+++ templates/form.php
@@ -2,7 +2,7 @@
 
 	<div class="input_group gif_group">
 		<span class="iconic camera"></span>
-		<label for="gif_url">Gif</label>
+		<label for="gif_url">Gif<span class="small">URL of your gif</span></label>
 		<input id="gifSource" type="text" name="gif_url" onclick="select()" />
 		<div id="plus">+</div>
 	</div>
@@ -11,7 +11,7 @@
 
 	<div class="input_group sound_group">
 		<span class="iconic play_alt"></span>
-		<label for="audio_url">Sound</label>
+		<label for="audio_url">Sound<span class="small">Youtube, MP3, Dailymotion...</span></label>
 		<input id="soundSource" type="text" name="audio_url" onclick="select()" />
 	</div>
 
