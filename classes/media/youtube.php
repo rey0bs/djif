2e54e6d14607d2bedf8d308a9b9c415e07ca2c2c
Simon Boyer <simon.boyer@openwide.fr>
2013-05-02 17:02:15 +0200

    Support youtube start offset

--- classes/media/youtube.php
+++ classes/media/youtube.php
@@ -7,9 +7,44 @@ class Youtube extends Media {
 	public static function isMine( $url ) {
 		return preg_match("#(\.|/)youtu\.?be(\.|/)#i", $url);
 	}
-
+	
 	public function getHash( ) {
 		return preg_replace( '#.*(v=|youtu.be/)([^&\?]+)&?.*#', '$2', $this->url );
+	}
+	
+	public function getPlaceholders() {
+		return array(
+				'keys' => array(
+						'[[hash]]',
+						'[[start]]'
+				),
+				'values' => array(
+						$this->getHash(),
+						$this->getStart()
+				)
+		);
+	}
+	
+	public function getStart() {
+		$time = $this->getTime();
+		if ($time) {
+			return '&start='.$time;
+		} else {
+			return '';
+		}
+		
+	}
+	public function getTime() {
+	
+		$time = preg_replace( '/.*(&|\?|#)t=([0-9m]*)s?.*/', '$2', $this->url );
+	
+		$time = explode('m', $time);
+		if (count($time)>1) {
+			$time = $time[0]*60+$time[1];
+		} else {
+			$time = $time[0];
+		}
+		return $time;
 	}
 }
 ?>
