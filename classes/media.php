e8dc7e7af07b46706e32d8964909069f9e1326a8
gbrenon <gregoire.brenon@openwide.fr>
2013-05-06 10:40:18 +0200

    Removed 'proxy' function that resolved urls before displaying the djif

--- classes/media.php
+++ classes/media.php
@@ -50,53 +50,19 @@ class Media {
 		return $output;
 	}
 	
-	public function resolve($url) {
-		$headers = @get_headers($url);
-		if(preg_match("#^HTTP/[0-9]\.[0-9] *([0-9]+).*$#i", $headers[0], $code)) {
-			switch ($code[1]) {
-				case 200: // OK
-					return $url;
-				case 404: // NOT FOUND
-					return false;
-				case 302: // Moved
-				case 303:
-					foreach($headers as $line) {
-						if(preg_match("#^Location *: *(http.*)$#i", $line, $location)) {
-							$new_url = $location[1];
-							break;
-						}
-					}
-					if ($new_url) {
-						return $this->resolve($new_url);
-					} else {
-						return false;
-					}
-				default:
-					error_log("New HTTP return code to handle : $code[1] encountered at '$url'");
-					return false;
-			}
-		} else {
-			return false;
-		}
-	}
-
 	public function getMedia() {
-	
-		$url = $this->resolve($this->url);
-		if ($url) {
-			// load classes
-			foreach (glob("classes/media/*.php") as $path) {
+		// load classes
+		foreach (glob("classes/media/*.php") as $path) {
 
-				require_once $path;
-				$filename = explode("/", $path);
-				$filename = $filename[count($filename)-1];
+			require_once $path;
+			$filename = explode("/", $path);
+			$filename = $filename[count($filename)-1];
 
-				$t = explode(".", $filename);
-				$class = ucfirst($t[count($t)-2]);
+			$t = explode(".", $filename);
+			$class = ucfirst($t[count($t)-2]);
 
-				if ($class::isMine($url)) {
-					return new $class($url);
-				}
+			if ($class::isMine($this->url)) {
+				return new $class($this->url);
 			}
 		}
 		return false;
