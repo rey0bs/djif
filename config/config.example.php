386369e651b3f93011db81b7254fb494bb8096ee
gbrenon <gregoire.brenon@openwide.fr>
2013-05-06 14:17:43 +0200

    Added script to reset the database + number of visits is now counted

--- config/config.example.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DB_NAME','mydb');
-define('DB_USER','user');
-define('DB_PASS','pass');
-define('DB_HOST','somewhe.re');
-define('DB_PORT','42'); // it always has to be 42
-?>
---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_HOST','');
+define('DB_PORT','');
+define('DB_NAME','');
+define('DB_USER','');
+define('DB_PASS','');
+?>
