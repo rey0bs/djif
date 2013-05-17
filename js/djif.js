424b8e486e4810b101e82443c3c4973636a1b6fe
gbrenon <gregoire.brenon@openwide.fr>
2013-05-17 15:39:06 +0200

    Removed dead code (there once was a time when form wouldn't disappear and multiple submit would be possible)

--- js/djif.js
+++ js/djif.js
@@ -48,8 +48,6 @@ $(function(){
 			$('#tooltip').fadeIn(100);
 			return false;
 		} else {
-			$('#screen').remove();
-			$('#speaker').remove();
 			$.ajax({
 				type: "POST",
 				url: "/ajax/new",
