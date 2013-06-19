4854c9ac4162b042f99d8769ddd36b785f048f15
gbrenon <gregoire.brenon@openwide.fr>
2013-06-19 17:46:00 +0200

    Nice buttons at the top (all directing to random for now)

--- templates/includes/header.php
+++ templates/includes/header.php
@@ -24,5 +24,8 @@
 		<div id="page">
 			<h1 id="logo"><a href="/">DJif</a></h1>
 			<?php include($lead); ?>
+			<a class="button rond" href="/wtf"><span class="iconic heart_stroke"></span></a>
+			<a class="button rond" href="/wtf">?</a>
+			<a class="button rond" href="/wtf"><span class="iconic clock"></span></a>
 			<div id="action">
 
