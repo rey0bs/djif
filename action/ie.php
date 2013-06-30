f1fbc6bcb3457c798d5640078f95d6acda632a5b
Greygjhart <greg+devel@marvid.fr>
2013-06-30 11:55:57 +0200

    Minor language fixes in ie.php

--- action/ie.php
+++ action/ie.php
@@ -2,8 +2,8 @@
 	<strong>Did you know that your Internet Explorer is out of date?</strong>
 </p>
 <p class="quote text-left">
-To get the best possible experience using our website we recommend that you upgrade to a newer version or other web browser. A list of the most popular web browsers can be found below.<br />
-Just click on the links to get to the download page :</p>
+To get the best experience possible using our website we recommend that you upgrade to a newer version or another web browser. A list of the most popular web browsers can be found below.<br />
+Just click on one of the links to get to the download page :</p>
 <p>
 	<a class="button" href="http://www.microsoft.com/windows/Internet-explorer/default.aspx">Internet Explorer 10+</a>
 	<a class="button" href="http://www.mozilla.com/firefox/">Firefox</a>
