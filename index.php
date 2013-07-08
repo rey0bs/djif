580e545a9edd0f83d09255b5f36e5fac52635486
gbrenon <gregoire.brenon@openwide.fr>
2013-07-08 12:32:59 +0200

    Added piwik support !

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 	include('index_route.php');
 	include('templates/includes/header.php');
 	$ajax = false;
+	include('templates/piwik.html');
 	include($page);
 	include('templates/includes/footer.php');
 ?>
--- index_ajax.php
+++ index_ajax.php
@@ -1,5 +1,6 @@
 <?php
 	include('index_route.php');
 	$ajax = true;
+	include('templates/piwik.html');
 	include($page);
 ?>
