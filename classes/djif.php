b0db6fdf674ca1a16f7eed6f5503a55d3d125ecd
gbrenon <gregoire.brenon@openwide.fr>
2013-07-18 16:54:58 +0200

    One more shameful commit

--- classes/djif.php
+++ classes/djif.php
@@ -114,7 +114,7 @@ class Djif {
 				ACCESS_TOKEN_SECRET
 		);
 		
-		$title = str_replace('_', ' ', $this->title;)
+		$title = str_replace('_', ' ', $this->title);
 		if (strlen($title) > 140-23) {
 			$title = substr($title, 0, 140-26).'...';
 		}
