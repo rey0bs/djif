54a920415df16a1f0c42658eeca0446fd7a595f0
Simon Boyer <simon.boyer@openwide.fr>
2014-04-11 16:50:01 +0200

    Add config sample

---
+++ config/config.sample.php
@@ -0,0 +1,17 @@
+<?php
+define('DB_HOST','');
+define('DB_PORT','');
+define('DB_NAME','');
+define('DB_USER','');
+define('DB_PASS','');
+
+define('CONTACT','my@address.net');
+
+define('SEQUENCE_SIZE', 10);
+
+define('CONSUMER_KEY', 'twitter');
+define('CONSUMER_SECRET', 'twitter');
+define('ACCESS_TOKEN', 'twitter');
+define('ACCESS_TOKEN_SECRET', 'twitter');
+
+?>
